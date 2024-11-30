c9abf462b4c3f50c07c5542e5763ca147c72270a
Travis Briggs <audiodude@gmail.com>
2024-11-30 13:38:25 -0800

    Working counter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,63 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import Headline from './components/Headline.vue';
+
+export default {
+  name: 'App',
+  components: { Headline },
+  data() {
+    return {
+      topMsg: 'Searching for Abbys...',
+      numAbbys: '',
+      abbyCount: 5555,
+    };
+  },
+  mounted() {
+    this.startAnimation();
+  },
+  methods: {
+    startAnimation() {
+      const MAX_DELAY = 500;
+      const MAX_NUM_DELAYS = 4;
+
+      let numDelays = 0;
+      let delay = 0;
+      let delayDirection = 1;
+      const updateNum = () => {
+        this.abbyCount += Math.floor(Math.random() * this.abbyCount * 0.08);
+        this.numAbbys = `Found ${this.abbyCount} Abbys in the Universe`;
+        if (this.abbyCount < 100_000_000_000) {
+          const timeout = Math.floor(Math.random() * 30);
+          if (numDelays < MAX_NUM_DELAYS && Math.random() > 0.99) {
+            numDelays += 1;
+            if (delayDirection === 1) {
+              delay = 50;
+            } else {
+              delay = MAX_DELAY;
+            }
+          }
+          if (delay > MAX_DELAY) {
+            delay = MAX_DELAY;
+            delayDirection = -1;
+          } else if (delay < 15) {
+            delay = 0;
+            delayDirection = 1;
+          }
+          const scale = Math.pow(1.7, delayDirection);
+          delay = delay + delay * scale * delayDirection;
+          setTimeout(updateNum, timeout + delay);
+        }
+      };
+
+      updateNum();
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1 class="text-pink-500">bestabbyintheuniverse.com</h1>
+    <Headline :msg="topMsg"></Headline>
+    <Headline v-if="numAbbys" :msg="numAbbys"></Headline>
   </main>
 </template>
 
---
+++ src/components/Headline.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: 'Headline',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1 class="text-4xl text-center my-8">{{ msg }}</h1>
+  </div>
+</template>
+
+<style></style>
